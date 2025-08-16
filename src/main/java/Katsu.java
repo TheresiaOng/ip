b2520d37ecf12dda9b8407e9be13ceb155730b22
Theresia <e1375477@u.nus.edu>
2025-08-16 16:27:30 +0800

    Complete Level-0
    Rename Duke > Katsu
    Implement an initial skeletal version of Katsu
    that simply greets the user and exits

---
+++ src/main/java/Katsu.java
@@ -0,0 +1,19 @@
+public class Katsu {
+    public static void main(String[] args) {
+        String name = "Katsu";
+        String seperator = "____________________________________________________________";
+        String logo = " _  __     _             \n"
+                + "| |/ /__ _| |_ ___ _   _ \n"
+                + "| ' // _` | __/ __| | | |\n"
+                + "| . \\ (_| | |_\\__ \\ |_| |\n"
+                + "|_|\\_\\__,_|\\__|___/\\__,_|\n";
+
+        System.out.println(seperator);
+        System.out.println(logo);
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println(seperator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(seperator);
+    }
+}

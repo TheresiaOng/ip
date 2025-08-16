d63fd82892fbbe4c66128b73de8d3e76e83bdc83
Theresia <e1375477@u.nus.edu>
2025-08-16 16:50:19 +0800

    Complete Level-1
    Katsu can now echos user's input and exits
    when given the command "bye"

--- src/main/java/Katsu.java
+++ src/main/java/Katsu.java
@@ -1,19 +1,37 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Katsu {
     public static void main(String[] args) {
-        String name = "Katsu";
-        String seperator = "____________________________________________________________";
-        String logo = " _  __     _             \n"
-                + "| |/ /__ _| |_ ___ _   _ \n"
-                + "| ' // _` | __/ __| | | |\n"
-                + "| . \\ (_| | |_\\__ \\ |_| |\n"
-                + "|_|\\_\\__,_|\\__|___/\\__,_|\n";
+        boolean active = true;
+        String name = "Katsu the Duck";
+        String indent = "    ";
+        String seperator = indent + "____________________________________________________________";
+        String logo = indent + " _  __     _             \n"
+                + indent + "| |/ /__ _| |_ ___ _   _ \n"
+                + indent + "| ' // _` | __/ __| | | |\n"
+                + indent + "| . \\ (_| | |_\\__ \\ |_| |\n"
+                + indent + "|_|\\_\\__,_|\\__|___/\\__,_|\n";
 
         System.out.println(seperator);
         System.out.println(logo);
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
-        System.out.println(seperator);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(seperator);
+        System.out.println(indent + "Hello! I'm " + name);
+        System.out.println(indent + "What can I do for you?");
+        System.out.println(seperator +"\n");
+
+        Scanner scanner = new Scanner(System.in);
+        while (active) {
+            String order = scanner.nextLine();
+            System.out.println(seperator);
+
+            if (Objects.equals(order, "bye")) {
+                System.out.println(indent + "Quack. Hope to see you again soon!");
+                active = false;
+            } else {
+                System.out.println(indent + "Quack " + order);
+            }
+
+            System.out.println(seperator + "\n");
+        }
     }
 }
